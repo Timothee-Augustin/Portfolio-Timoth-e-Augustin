0b92a994c0c130fdc80c679535a5e82e8d9321a4
Timothee <augustin.timothee@gmail.com>
2021-07-21 14:35:50 +0200

    install/config eslint and dotenv

---
+++ back/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const connection = require('./db-config');
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+  } else {
+    console.log(`connected to database with threadId :  ${connection.threadId}`);
+  }
+});
+
+app.use(express.json());
+
+app.use('/uploads', express.static('uploads'));
+app.use('/assets', express.static('assets'));
+
+// const routes = require('./routes');
+
+// app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
