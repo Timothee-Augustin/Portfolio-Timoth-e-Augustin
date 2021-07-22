27280fcc78242e8147d529a5b45b801432054855
Timothee <augustin.timothee@gmail.com>
2021-07-22 16:44:47 +0200

    added a new route in techno.js

--- back/routes/technos.js
+++ back/routes/technos.js
@@ -1,6 +1,19 @@
 const technoRoute = require('express').Router();
 const connection = require('../db-config');
 
+technoRoute.get('/', (req, res) => {
+  connection.query(
+    'SELECT * FROM techno ',
+    (err, results) => {
+      if (err) {
+        res.status(500).send('Error retrieving the technos from database');
+      } else {
+        res.json(results);
+      }
+    },
+  );
+});
+
 technoRoute.get('/:id', (req, res) => {
   const projectId = req.params.id;
   connection.query(
